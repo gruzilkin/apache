30a3f0b33bd3e464f1072fd4a4f26448cda3eb6d
Sergey <gruzmob@gmail.com>
2022-05-31 13:50:08 +0000

    ur mom can use memcached now

--- web/html/index.php
+++ web/html/index.php
@@ -4,15 +4,29 @@ $db_name = getenv('DB_NAME');
 $db_user = getenv('DB_USER');
 $db_pass = getenv('DB_PASS');
 
-$dsn = "pgsql:dbname={$db_name};host={$db_host}";
+$memcached_host = getenv('MEMCACHED_HOST');
 
-$db = new PDO($dsn, $db_user, $db_pass);
+$memcached = new Memcached();
+$memcached->addServer($memcached_host, 11211);
+$message = $memcached->get("urmom");
 
-$sql = 'SELECT sender, contents FROM messages';
+if ($message) {
+	echo "$message from memcached";
+} else {
+	$dsn = "pgsql:dbname={$db_name};host={$db_host}";
 
-foreach ($db->query($sql) as $row) {
-  $sender = $row['sender'];
-  $contents = $row['contents'];
-  echo "$sender says $contents";
+	$db = new PDO($dsn, $db_user, $db_pass);
+
+	$sql = 'SELECT sender, contents FROM messages';
+
+	foreach ($db->query($sql) as $row) {
+	  $sender = $row['sender'];
+	  $contents = $row['contents'];
+	  $message = "$sender says $contents";
+	  
+	  echo "$message from db";
+	  
+	  $memcached->set("urmom", $message);	  
+	}
 }
 ?>
